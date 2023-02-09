5ac17140d6f29199b06cf1a461a8168523ac38b7
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2023-02-09 17:48:38 -0500

    ADD: Código del controlador para la creación de usuarios.

--- api/src/controller/usersController.js
+++ api/src/controller/usersController.js
@@ -1,6 +1,17 @@
 const User = require('../models/userSchema');
 const buildParams=require('./helpers').builParams;
 
-async function create(req,res){
 
+const validParams=['name','email', 'password'];
+//1ero probaré una función síncrona:
+function create(req,res){
+    let params=buildParams(validParams, req.body);
+
+    User.create(params).then(user=>{
+        res.json(user);
+    }).catch(err=>{
+        console.log(err);
+    });
 }
+
+module.exports={create}
