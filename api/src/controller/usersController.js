8e4058d1f033c44eaa2e3b872cdce9bc5c1a1093
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2023-02-10 00:13:58 -0500

    EDIT: Formato al código.

--- api/src/controller/usersController.js
+++ api/src/controller/usersController.js
@@ -5,9 +5,9 @@ const validParams = ['name', 'email', 'password'];
 //Crear usuario
 const createUser = async (req, res) => {
     let params = buildParams(validParams, req.body);
+    console.log(params);
     try {
-        let usuario=await userSchema.create(params);
-        console.log(usuario);
+        let usuario = await userSchema.create(params);
         res.send(usuario);
     } catch (error) {
         console.log(error);
