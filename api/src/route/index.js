88d830705e0ae0004b1a62aa3c02b88c99a43bf0
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-12 16:01:51 -0300

    modulo de envio mai v2

--- api/src/route/index.js
+++ api/src/route/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 const app = express();
 import routeHome from "../route/home.js";
-import classroom from "../route/classroomRoute.js"
+import classroom from "../route/classroomRoute.js";
 
 function routerApi(app) {
   app.use("/api", routeHome);
-  app.use("/classroom", classroom)
- 
+  app.use("/classroom", classroom);
 }
 
 export default routerApi;
