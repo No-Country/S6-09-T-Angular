dfd21ce156b7152d5a11de7501361f0ae8411fbb
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2023-02-12 22:07:30 -0500

    REFACTOR: Se cambió el nombre de: "senEmail" --> "sendEmail" y de "correo" --> "email".

--- api/src/helpers/email.js
+++ api/src/helpers/email.js
@@ -1,16 +1,16 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
-import sgMail  from '@sendgrid/mail'
+import sgMail from '@sendgrid/mail'
 /*sendEmail funciona con dos parametros el usuario y un string que puede variar segun el caso
 bienvenida, despedida, invitcion */
-const senEmail=(user,template)=>{
+const sendEmail = (user, template) => {
 
-let templates=[{
-  id:"bienvenida",
-  subject: 'Bienvenido a ClasseMote',//rellenar con objeto datos
-  text: 'Este es un texto de prueba',
-  html:`
+  let templates = [{
+    id: "bienvenida",
+    subject: 'Bienvenido a ClasseMote',//rellenar con objeto datos
+    text: 'Este es un texto de prueba',
+    html: `
 <html>
   <head>
     <title>hola</title>
@@ -21,11 +21,11 @@ let templates=[{
   </body>
 </html>
 `
-},{
-  id:"despedida",
-  subject: 'Sending with SendGrid is Fun',//rellenar con objeto datos
-  text: 'and easy to do anywhere, even with Node.js',
-  html:`
+  }, {
+    id: "despedida",
+    subject: 'Sending with SendGrid is Fun',//rellenar con objeto datos
+    text: 'and easy to do anywhere, even with Node.js',
+    html: `
 <html>
   <head>
     <title>hola</title>
@@ -36,11 +36,27 @@ let templates=[{
   </body>
 </html>
 `
-},{
-  id:"invitacion",
-  subject: 'Sending with SendGrid is Fun',//rellenar con objeto datos
-  text: 'and easy to do anywhere, even with Node.js',
-  html:`
+  },
+  {
+    id: "holaK",
+    subject: 'Probando, probando',
+    text: 'A ver.',
+    html: `
+<html>
+  <head>
+    <title>hola</title>
+  </head>
+  <body>
+    <p>Probando correo para Kim</p>
+    <p>This is template 1.</p>
+  </body>
+</html>
+`
+  }, {
+    id: "invitacion",
+    subject: 'Sending with SendGrid is Fun',//rellenar con objeto datos
+    text: 'and easy to do anywhere, even with Node.js',
+    html: `
 <html>
   <head>
     <title>hola</title>
@@ -51,25 +67,31 @@ let templates=[{
   </body>
 </html>
 `
-}
-]
+  }
+  ]
 
-let datos= templates.filter((dato)=>dato.id === template)
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  //let datos = templates.filter((dato) => dato.id === template)
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  console.log(process.env.SENDGRID_API_KEY);
+  console.log(user);
 
-const msg = {
-  to: 'yannokaiserfrom@gmail.com', // email del emisor
-  from:  `${user.correo} `, // email del receptor user.email
-  subject: datos[0].subject,//rellenar con objeto datos
-  text: datos[0].text,//rellenar con objeto datos
-  html: `${datos[0].html} `,//body
-}
-try {
+  const msg = {
+    to: 'yannokaiserfrom@gmail.com', // email del emisor
+    //from: `${user.email} `, // email del receptor user.email
+    from: 'cbrnka1997@duck.com',
+    //subject: datos[0].subject,//rellenar con objeto datos
+    subject: "Ayuda",
+    //text: datos[0].text,//rellenar con objeto datos
+    text: "Hola, según Migue.",
+    //html: `${datos[0].html} `,//body
+    html: "<h1>Hola</h1>"
+  }
+  try {
     sgMail.send(msg)
     console.log('Email sent')
-} catch (error) {
-    console.error(error)
-}
+  } catch (error) {
+    console.error(error.message)
+  }
 }
 
-export {senEmail}
+export { sendEmail }
