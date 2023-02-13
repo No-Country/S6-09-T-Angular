86dac511b1a500fe443023caca27d90d553b9ca6
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2023-02-12 22:05:46 -0500

    ADD: Frase de Migue.

--- api/src/route/usersRoute.js
+++ api/src/route/usersRoute.js
@@ -4,6 +4,9 @@ import { createUser } from '../controller/usersController.js';
 const app = express();
 
 //Crear un usuario
-app.post("/", createUser);
+app.post("/", createUser); 
 
 export default app;
+
+//No importa que antos principios o reglas te digan que tiene un hombre, si pasa una mujer linda el hombre volteará.
+//Atte.: Miguel Guerrero.
