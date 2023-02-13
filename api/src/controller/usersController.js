df2c4d7d107281e1264269bacf2a3a781772e7ff
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-13 02:58:37 -0300

    Validator para create user

--- api/src/controller/usersController.js
+++ api/src/controller/usersController.js
@@ -2,13 +2,19 @@ import userSchema from '../models/userSchema.js';
 import { buildParams } from './helpers.js';
 import {enviar} from '../helpers/email.js';
 
+
 const validParams = ['name', 'email', 'password'];
 //Crear usuario
 const createUser = async (req, res) => {
+    
     let params = buildParams(validParams, req.body);
     console.log(params);
+
+
     try {
         let user = await userSchema.create(params);
+        user.password=undefined
+        res.send(user)
         enviar(user,"bienvenida");
     } catch (error) {
         console.log(error);
