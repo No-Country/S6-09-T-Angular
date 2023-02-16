de394e15b7dcd98fe967fe1ba1fc50bc1388b9e4
leonelpb <leonelperez.b23@gmail.com>
2023-02-16 17:35:02 -0300

    Fix routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomePageComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
