25618c7ae4b8147184c58610a3a6cae4432add0e
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-17 01:08:42 -0300

    Terminado el controler de user

--- api/src/route/usersRoute.js
+++ api/src/route/usersRoute.js
@@ -1,19 +1,22 @@
 import express from "express";
-import { createUser } from "../controller/usersController.js";
+import { createUser,getUsers,updateUser, deleteUser,getUser } from "../controller/usersController.js";
 import { validationGeneral } from "../validator/validationGeneral.js";
 import { rulesUser } from "../validator/validationUser.js";
+import {rulesUserupdate} from "../validator/validationUpdateUser.js"
 
 const app = express();
 
 //Crear un usuario y su token
 app.post("/", rulesUser, validationGeneral, createUser);
 
-//Login del usuario
-
+//get allUser
+app.get("/", getUsers);
+//get usuario
+app.get("/:id",rulesUserupdate,validationGeneral, getUser );
 //Delete usuario
-
+app.patch("/:id",rulesUserupdate,validationGeneral,updateUser);
 //Update usuario
-
+app.delete("/:id",rulesUserupdate,validationGeneral, deleteUser);
 export default app;
 
 //No importa que antos principios o reglas te digan que tiene un hombre, si pasa una mujer linda el hombre volteará.
