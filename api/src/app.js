7bf533d50b17664b6d1764d0b29f8309c2631932
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-17 02:46:13 -0300

    Corregido schema de user

--- api/src/app.js
+++ api/src/app.js
@@ -10,14 +10,14 @@ const server = http.createServer(app);
 const io = new SocketIO(server);
 import { coneccionSocket } from "../src/socket/socket.js";
 import connectdb from "./config/db.js";
-//import cors from "cors"; //enable cors
+import cors from "cors"; //enable cors
 //configuracion de archivos staticos
 app.use(express.static("./src/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Configuracion de cors
-//app.use(cors());
+app.use(cors());
 //rutas de la api
 routerApi(app);
 //conecciones
--- api/src/public/zumbido.js
+++ api/src/public/zumbido.js
@@ -1,7 +1,34 @@
 let boton = document.getElementById("zumbido");
+let users= document.getElementById("users")
 const audio = new Audio("../audio/1.mp3");
 boton.addEventListener("click", () => {
   console.log("hola mundo");
   audio.play();
   window.navigator.vibrate([100]);
 });
+
+const datos=async()=>{
+  let url="http://localhost:3000/classroom/all"
+  let datos= await fetch(url)
+  return await datos.json()
+}
+
+const pintarUsers=async()=>{
+  let user=await datos()
+  console.log(user);
+  user.forEach(element => {
+    let tpl=`
+  id=${element.user_id}
+  user=${element.classroom_name} 
+  <br>
+  <br>
+
+  `  
+  users.innerHTML+=tpl
+  });
+  
+  
+
+}
+ pintarUsers()
+
