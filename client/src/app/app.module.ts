cd514d8c7fd56ab2b91b293fb23e0b743d0528fc
leonelpb <leonelperez.b23@gmail.com>
2023-02-18 17:51:03 -0300

    Fix error

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassRoomComponent } from "./class-room/class-room.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    NavbarComponent,
-    HomePageComponent,
-    NosotrosComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ClassRoomComponent,
+        NavbarComponent,
+        HomePageComponent,
+        NosotrosComponent,
+        FooterComponent,
+
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ]
 })
 export class AppModule { }
