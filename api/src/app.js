834aacb8c3b63bcfe7c045dc8e4e9564c01acd7a
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2023-02-20 21:45:22 -0500

    ADD: Se añadió el uso de "express-jwt" (para saber si un token es válido o no.)

--- api/src/app.js
+++ api/src/app.js
@@ -1,17 +1,24 @@
 import * as dotenv from "dotenv";
 import express, { json } from "express";
-const app = express();
-const port = 3000;
 import { Server as SocketIO } from "socket.io";
 import http from "http";
 import { Stream } from "stream";
 import routerApi from "./route/index.js";
-const server = http.createServer(app);
-const io = new SocketIO(server);
 import { coneccionSocket } from "../src/socket/socket.js";
 import connectdb from "./config/db.js";
 import cors from "cors"; //enable cors
 import jwtMiddleware from "express-jwt";
+
+const { expressjwt } = jwtMiddleware;
+const app = express();
+const port = 3000;
+const server = http.createServer(app);
+const io = new SocketIO(server);
+
+//Config. de express-jwt (todas las peticiones requerirán que se les envíe un token)
+app.use(expressjwt({ secret: process.env.jwtSecret, algorithms: ["HS256"] }).unless(
+  { path: ['/login', '/users'] }) //evita que estas rutas estén protegidas por el token
+);
 
 //configuracion de archivos staticos
 app.use(express.static("./src/public"));
@@ -23,7 +30,6 @@ app.use(cors());
 //rutas de la api
 routerApi(app);
 //conecciones
-
 coneccionSocket(io, app);
 server.listen(port, () => {
   console.log("Server corriendo en " + port);
