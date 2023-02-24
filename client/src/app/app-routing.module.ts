88ff3a8d24bf5ad635d0e82105ea64e5304d0987
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-02-24 18:25:34 -0300

    correccion merge

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ClassRoomComponent } from './class-room/class-room.component';
+//import { ClassRoomComponent } from './class-room/class-room.component';
 import { ClassWallComponent } from './class-room/class-wall/class-wall.component';
 import { ClassDeliveriesComponent } from './class-room/class-deliveries/class-deliveries.component';
 import { ClassMembersComponent } from './class-room/class-members/class-members.component';
 
 const routes: Routes = [
-  {
-    path: 'classroom',
-    component: ClassRoomComponent,
-  },
+  // {
+  //   path: 'classroom',
+  //   component: ClassRoomComponent,
+  // },
   {
     path: 'classroom/class-wall',
     component: ClassWallComponent,
