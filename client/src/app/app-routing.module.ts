d593c2774445ffd0a6d414c02c189143db098319
leonelpb <leonelperez.b23@gmail.com>
2023-02-24 00:06:40 -0300

    fix 3

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,37 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-<<<<<<< HEAD
-import { ClassRoomComponent } from './class-room/class-room.component';
-import { ClassWallComponent } from './class-room/class-wall/class-wall.component';
-import { ClassDeliveriesComponent } from './class-room/class-deliveries/class-deliveries.component';
-import { ClassMembersComponent } from './class-room/class-members/class-members.component';
 
-const routes: Routes = [
-  {
-    path:'classroom',
-    component:ClassRoomComponent
-  },
-  {
-    path:'classroom/class-wall',
-    component:ClassWallComponent
-  },
-  {
-    path:'classroom/class-deliveries',
-    component:ClassDeliveriesComponent
-  },
-  {
-    path:'classroom/class-members',
-    component:ClassMembersComponent
-  },
-  {
-=======
-//import { ClassRoomComponent } from './class-room/class-room.component';
 
 
 const routes: Routes = [
   {
->>>>>>> 1082f417f758d0b084a51dbd33f6850e235e7b7e
     path:'inicio',
     component:HomePageComponent
   },
