0b3ad3ed8a5bda3d86114c9c6758ade75b924c8e
Franciscozcode <zzpanch@gmail.com>
2023-02-28 01:38:34 -0300

    componente de francisco

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PerfilComponent } from './protected/pages/perfil/perfil.component';
 //import { ClassRoomComponent } from './class-room/class-room.component';
 
 
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path:'nosotros',
     component:NosotrosComponent
+  },
+  {
+    path:'perfil',
+    component:PerfilComponent
   },
   {
     path:'auth',
