6e19a0c10cce2531c442d0aaa851221a263f1478
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-03-03 12:09:58 -0300

    solucionando marge

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:3001',
+  production: true,
+  // baseUrl: 'http://localhost:3001',
+  baseUrl: 'https://angula-production.up.railway.app',
 };
 
 /*
